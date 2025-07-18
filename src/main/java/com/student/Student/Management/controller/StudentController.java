f0cc2da71d1d790b7704f11cf71af486a1d06189
Navin kumar <navin1sepkumar@gmail.com>
2025-07-18 19:15:38 +0530

    security added checked

--- src/main/java/com/student/Student/Management/controller/StudentController.java
+++ src/main/java/com/student/Student/Management/controller/StudentController.java
@@ -4,17 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.student.Student.Management.entity.Student;
 import com.student.Student.Management.repository.StudentRepository;
 
+@RestController
+@RequestMapping
 public class StudentController {
 
 	@Autowired
---
+++ src/main/java/com/student/Student/Management/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.student.Student.Management.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/students/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("navin").password("{noop}navin22").roles("USER");
+    }
+
+}
