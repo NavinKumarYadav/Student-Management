d51c176e31430e3b88d7068055a3f6d4dbcea221
Navin kumar <navin1sepkumar@gmail.com>
2025-07-18 16:21:56 +0530

    student managment

--- src/main/java/com/student/Student/Management/controller/StudentController.java
+++ src/main/java/com/student/Student/Management/controller/StudentController.java
@@ -1,4 +1,57 @@
 package com.student.Student.Management.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.student.Student.Management.entity.Student;
+import com.student.Student.Management.repository.StudentRepository;
+
 public class StudentController {
+
+	@Autowired
+	StudentRepository repo;
+
+	@GetMapping("/students")
+	public List<Student> getAllStudents() {
+
+		List<Student> students = repo.findAll();
+
+		return null;
+	}
+
+	@GetMapping("/students/{id}")
+	public Student getStudent(@PathVariable int id) {
+		Student student = repo.findById(id).get();
+
+		return student;
+	}
+
+	@PostMapping("/student/add")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void createStudent(@RequestBody Student student) {
+		repo.save(student);
+
+	}
+
+	@PutMapping("/student/update/{id}")
+	public Student updateStudent(@PathVariable int id) {
+		Student student = repo.findById(id).get();
+
+		return student;
+	}
+
+	@DeleteMapping("/student/delete/{id}")
+	public void deleteStudent(@PathVariable int id) {
+		Student student = repo.findById(id).get();
+		repo.delete(student);
+	}
 }
